8a33d70321bf9333bac8f73f174a3ae0f1a57d96
Aliot26 <olgakochanowa@gmail.com>
2018-11-18 13:53:08 +0100

    added function calculate_result

--- bowling.py
+++ bowling.py
@@ -13,26 +13,26 @@ def score(game):
         # if not in_first_half:
             # frame += 1
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+            result = calculate_result(game, result, i)
         last = get_value(game[i])
 
         if not in_first_half:
             frame += 1
         in_first_half = reverse(in_first_half)
-        """if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True"""
+
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+    return result
+
+
+def calculate_result(game, result, i):
+    result += get_value(game[i+1])
+    if game[i] == 'X' or game[i] == 'x':
+        if game[i+2] == '/':
+            result += 10 - get_value(game[i+1])
+        else:
+            result += get_value(game[i+2])
     return result
 
 
@@ -45,9 +45,7 @@ def reverse(flag):
 def get_value(char):
     if char in game_list:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
